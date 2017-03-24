5a005dcd7f272b31de89919e5f9c1efa5832bd6b
shinkira <shinkira3@gmail.com>
2017-03-24 10:14:03 -0400

    parseScanimageTiff minor update

--- common/Motion Correction Files/parseScanimageTiff.m
+++ common/Motion Correction Files/parseScanimageTiff.m
@@ -13,7 +13,8 @@ if isfield(siStruct, 'VERSION_MAJOR') && strcmp(siStruct.VERSION_MAJOR, '2016')
         nSlices     = siStruct.hFastZ.numFramesPerVolume; % Slices are acquired at different locations (e.g. depths).
     else
         nSlices     = 1;
-    end 
+    end
+    siStruct.fastZDiscardFlybackFrames = siStruct.hFastZ.discardFlybackFrames;
 elseif isfield(siStruct, 'SI4')
     siStruct = siStruct.SI4;
     % Nomenclature: frames and slices refer to the concepts used in
@@ -21,7 +22,6 @@ elseif isfield(siStruct, 'SI4')
     fZ              = siStruct.fastZEnable;
     nChannels       = numel(siStruct.channelsSave);
     nSlices         = siStruct.stackNumSlices + (fZ*siStruct.fastZDiscardFlybackFrames); % Slices are acquired at different locations (e.g. depths).
-    fastZDiscardFlybackFrames = siStruct.fastZDiscardFlybackFrames;
 elseif isfield(siStruct,'SI5')
      siStruct = siStruct.SI5;
     % Nomenclature: frames and slices refer to the concepts used in
@@ -29,22 +29,15 @@ elseif isfield(siStruct,'SI5')
     fZ              = siStruct.fastZEnable;
     nChannels       = numel(siStruct.channelsSave);
     nSlices         = siStruct.stackNumSlices + (fZ*siStruct.fastZDiscardFlybackFrames); % Slices are acquired at different locations (e.g. depths).
-    fastZDiscardFlybackFrames = siStruct.fastZDiscardFlybackFrames;
 elseif isfield(siStruct, 'SI') % scanimage 2015 file
     fZ              = siStruct.SI.hFastZ.enable;
     nChannels       = length(siStruct.SI.hChannels.channelSave);
     if fZ
         nSlices = siStruct.SI.hFastZ.numFramesPerVolume;
-        fastZDiscardFlybackFrames = siStruct.SI.hFastZ.discardFlybackFrames;
+        siStruct.fastZDiscardFlybackFrames = siStruct.SI.hFastZ.discardFlybackFrames;
     else
         nSlices = 1;
     end
-elseif isfield(siStruct,'VERSION_MAJOR') && strcmp(siStruct.VERSION_MAJOR,'2016') % scanimage 2016 file
-    fZ              = siStruct.hFastZ.enable;
-    nChannels       = numel(siStruct.hChannels.channelSave);
-    stackNumSlices  = siStruct.hStackManager.numSlices;
-    fastZDiscardFlybackFrames = siStruct.hFastZ.discardFlybackFrames;
-    nSlices         = stackNumSlices + fZ*fastZDiscardFlybackFrames;
 elseif isfield(siStruct, 'software') && siStruct.software.version < 4 %ie it's a scanimage 3 file
     fZ = 0;
     nSlices = 1;
@@ -61,13 +54,13 @@ nCumFrame = (movNum-1) * size(mov,3); % cumulative sum of frames up to this movi
 firstInd = mod(nCumFrame+(1:nSet)-1,nSet) + 1; % Which frame set do the first nSet frames of the movie correspond to?
 
 if nSlices>1
-    for sl = 1:nSlices-(fZ*fastZDiscardFlybackFrames) % Slices, removing flyback.
+    for sl = 1:nSlices-(fZ*siStruct.fastZDiscardFlybackFrames) % Slices, removing flyback.
         for ch = 1:nChannels % Channels
             frameInd = find(firstInd==(ch + (sl-1)*nChannels),1,'first');
             movStruct.slice(sl).channel(ch).mov = mov(:, :, frameInd:(nSlices*nChannels):end);
         end
     end
-    nSlices = nSlices-(fZ*fastZDiscardFlybackFrames);
+    nSlices = nSlices-(fZ*siStruct.fastZDiscardFlybackFrames);
 else
     for sl = 1;
         for ch = 1:nChannels % Channels
