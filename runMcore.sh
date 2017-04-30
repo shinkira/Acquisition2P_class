0990592bef1e48d0cd6d64edfc7add3e8ac9cc83
shinkira <shinkira3@gmail.com>
2017-04-29 22:43:17 -0400

    runMcore.sh and runShort.sh added to run on Orchestra

---
+++ runMcore.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+run () {
+    
+	inputDataName=$1
+	
+	bsub -J $inputDataName -q mcore -W 240:00 -n 6 -r -R "rusage[mem=60000]" -o logs/MC_job%J.out /opt/matlab/bin/matlab -nodisplay -r "orchestra_execution_engine_v2(\$LSB_JOBID)"
+	}
+	
+#call arguments verbatim
+$@
