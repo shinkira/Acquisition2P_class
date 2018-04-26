e925109de5a11a6a0395b78ed48754c86de6405a
shinkira <shinkira3@gmail.com>
2018-04-26 13:46:38 -0400

    viewAcq updated for overview reference image

--- personal/Selmaan/viewAcq.m
+++ personal/Selmaan/viewAcq.m
@@ -1,15 +1,23 @@
 function [mov, mF] = viewAcq(obj,nSlice)
 
-if ~exist('nSlice','var')
-    nSlice = 1;
+    if ~exist('nSlice','var')
+        nSlice = 1;
+    end
+
+    movStruct = obj.correctedMovies.slice(nSlice).channel(1);
+
+    nMovies = length(movStruct.fileName);
+    mov = nan([movStruct.size(1,1:2),nMovies]);
+    for nMovie = 1:nMovies
+        t = meanRef(obj,nMovie,nSlice);
+        mov(:,:,nMovie) = nanmean(t,3);
+        % mov(:,:,nMovie) = imNorm(t);
+        mF(nMovie) = nanmean(t(:));
+    end
+
 end
 
-movStruct = obj.correctedMovies.slice(nSlice).channel(1);
-
-nMovies = length(movStruct.fileName);
-mov = nan([movStruct.size(1,1:2),nMovies]);
-for nMovie = 1:nMovies
-    t = meanRef(obj,nMovie,nSlice);
-    mov(:,:,nMovie) = imNorm(t);
-    mF(nMovie) = nanmean(t(:));
+function normImage = imNorm(rawImage)
+    rawImage = double(rawImage);
+    normImage = imadjust(rawImage/max(rawImage(:)));
 end
