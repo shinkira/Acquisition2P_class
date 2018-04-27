51b1e26c1313014321b3239e0a08c28d6577d686
shinkira <shinkira3@gmail.com>
2018-04-27 10:31:12 -0400

    wrapperUpload.sh updated

--- O2/build/wrapperUpload.sh
+++ O2/build/wrapperUpload.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160527/ /n/scratch2/sk574/Imaging/LT009/160527/
-rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160531/ /n/scratch2/sk574/Imaging/LT009/160531/
-rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160602/ /n/scratch2/sk574/Imaging/LT009/160602/
-rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160603/ /n/scratch2/sk574/Imaging/LT009/160603/
-rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160607/ /n/scratch2/sk574/Imaging/LT009/160607/
-rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160609/ /n/scratch2/sk574/Imaging/LT009/160609/
-rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160610/ /n/scratch2/sk574/Imaging/LT009/160610/
-rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160623/ /n/scratch2/sk574/Imaging/LT009/160623/
-rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160627/ /n/scratch2/sk574/Imaging/LT009/160627/
-rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT015/161214/ /n/scratch2/sk574/Imaging/LT015/161214/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160527/*.tif /n/scratch2/sk574/Imaging/LT009/160527/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160531/*.tif /n/scratch2/sk574/Imaging/LT009/160531/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160602/*.tif /n/scratch2/sk574/Imaging/LT009/160602/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160603/*.tif /n/scratch2/sk574/Imaging/LT009/160603/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160607/*.tif /n/scratch2/sk574/Imaging/LT009/160607/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160609/*.tif /n/scratch2/sk574/Imaging/LT009/160609/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160610/*.tif /n/scratch2/sk574/Imaging/LT009/160610/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160623/*.tif /n/scratch2/sk574/Imaging/LT009/160623/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160627/*.tif /n/scratch2/sk574/Imaging/LT009/160627/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT015/161214/*.tif /n/scratch2/sk574/Imaging/LT015/161214/
