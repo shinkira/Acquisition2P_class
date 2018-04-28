a5ae2bb5b646d1780810039383dc0382b624f10f
shinkira <shinkira3@gmail.com>
2018-04-27 22:18:21 -0400

    wrapperRunMedium.sh added

---
+++ O2/run/wrapperRunMedium.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160527
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160531
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160602
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160603
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160607
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160609
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160610
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160623
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160627
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 15 161214
