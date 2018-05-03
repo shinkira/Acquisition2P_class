e3b903cfa75230c9bbd79c28113acfc3d8e6914e
shinkira <shinkira3@gmail.com>
2018-05-02 23:07:55 -0400

    wrapper shell scripts updated for additional sessions for DA20  & VS35

--- O2/build/wrapperUpload.sh
+++ O2/build/wrapperUpload.sh
@@ -10,3 +10,7 @@ rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/16
 rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160623/*.tif /n/scratch2/sk574/Imaging/LT009/160623/
 rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT009/160627/*.tif /n/scratch2/sk574/Imaging/LT009/160627/
 rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/LT015/161214/*.tif /n/scratch2/sk574/Imaging/LT015/161214/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/DA020/161118/*.tif /n/scratch2/sk574/Imaging/DA020/161118/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/DA020/161121/*.tif /n/scratch2/sk574/Imaging/DA020/161121/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/DA020/161116/*.tif /n/scratch2/sk574/Imaging/DA020/161116/
+rsync -v -h -a /files/Neurobio/HarveyLab/Tier2/Shin/ShinDataAll/Imaging/VS035/170414/*.tif /n/scratch2/sk574/Imaging/VS035/170414/
--- O2/run/wrapperRunMedium.sh
+++ O2/run/wrapperRunMedium.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160527
-sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160531
-sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160602
-sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160603
-sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160607
-sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160609
-sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160610
-sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160623
-sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 9 160627
-sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 15 161214
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 20 161118
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 20 161121
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 20 161116
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2.sh 35 170414
