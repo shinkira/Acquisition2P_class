6ff4979059a9e80356e46f94145ba0862523d4cf
Shin Kira <shinkira@users.noreply.github.com>
2022-04-11 19:31:06 -0400

    Update runMediumO2.sh

--- O2/run/runMediumO2.sh
+++ O2/run/runMediumO2.sh
@@ -2,9 +2,9 @@
 
 #SBATCH -n 1                               # Request one core
 #SBATCH -N 1                               # Request one node
-#SBATCH -t 2-00:00                          # Runtime in D-HH:MM format
+#SBATCH -t 1-00:00                          # Runtime in D-HH:MM format
 #SBATCH -p medium                           # Partition to run in
-#SBATCH --mem=30000                        # Memory total in MB (for all cores)
+#SBATCH --mem=10000                        # Memory total in MB (for all cores)
 #SBATCH -o logs/logs_Acq2P/Acq2P_%A_%a.out   # File to which STDOUT will be written, including job ID
 #SBATCH -e errs/errs_Acq2P/Acq2P_%A_%a.err   # File to which STDERR will be written, including job ID
 #SBATCH --job-name=Acq2P
