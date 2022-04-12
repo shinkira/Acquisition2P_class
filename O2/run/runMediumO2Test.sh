cf564b6cb72c037623e477aaad196fb53e9a5cd8
Shin Kira <shinkira@users.noreply.github.com>
2022-04-12 08:56:50 -0400

    O2 test code added

---
+++ O2/run/runMediumO2Test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -n 1                               # Request one core
+#SBATCH -N 1                               # Request one node
+#SBATCH -t 1-00:00                          # Runtime in D-HH:MM format
+#SBATCH -p medium                           # Partition to run in
+#SBATCH --mem=10000                        # Memory total in MB (for all cores)
+#SBATCH -o logs/logs_Acq2P/Acq2P_%A_%a.out   # File to which STDOUT will be written, including job ID
+#SBATCH -e errs/errs_Acq2P/Acq2P_%A_%a.err   # File to which STDERR will be written, including job ID
+#SBATCH --job-name=Acq2P
+
+matlab -nodisplay -r "o2_execution_engine_acq2p_test(${SLURM_ARRAY_JOB_ID}, ${SLURM_ARRAY_TASK_ID}, $1, $2)"
---
+++ o2_execution_engine_acq2p_test.m
@@ -0,0 +1,10 @@
+function o2_execution_engine_acq2p_test(jobId, jobIndex, mouse_num, date_num)
+
+    try
+        fprintf('Hello World\n');
+        fprintf('%d %d %d %d\n',jobId, jobIndex, mouse_num, date_num);
+    catch
+        
+    end
+
+end
