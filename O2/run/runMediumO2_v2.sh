396235f816491fff062b1d9d2f77474bbea6496c
Shin Kira <shinkira@users.noreply.github.com>
2022-04-17 17:31:25 -0400

    Create runMediumO2_v2.sh

---
+++ O2/run/runMediumO2_v2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH -c 1                               # Request one core
+#SBATCH -t 1-00:00                          # Runtime in D-HH:MM format
+#SBATCH -p medium                           # Partition to run in
+#SBATCH --mem=10G                        # Memory total in MB (for all cores)
+#SBATCH -o logs/logs_Acq2P/Acq2P_%A_%a.out   # File to which STDOUT will be written, including job ID
+#SBATCH -e errs/errs_Acq2P/Acq2P_%A_%a.err   # File to which STDERR will be written, including job ID
+#SBATCH --job-name=Acq2P
+
+matlab -nodisplay -r "o2_execution_engine_acq2p(${SLURM_ARRAY_JOB_ID}, ${SLURM_ARRAY_TASK_ID}, $1, $2)"
