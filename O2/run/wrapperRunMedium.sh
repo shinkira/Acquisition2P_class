f247a64d5b47aca02751926d6680b88a08330ad2
Shin Kira <shinkira@users.noreply.github.com>
2022-04-17 17:31:28 -0400

    Update wrapperRunMedium.sh

--- O2/run/wrapperRunMedium.sh
+++ O2/run/wrapperRunMedium.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2Test.sh 101 220408
+sbatch --array=1 ~/GitHub/Acquisition2P_class/O2/run/runMediumO2Test.sh 101 220415
 
